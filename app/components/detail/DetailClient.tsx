9aae37381b97bc6b904c6c15482949e62c7443b2
ibrahimu ufuk kazdal <ibrahimufukkazdal@gmail.com>
2024-09-12 00:33:40 +0300

    button.tsx update

--- app/components/detail/DetailClient.tsx
+++ app/components/detail/DetailClient.tsx
@@ -3,6 +3,8 @@
 import { useState } from "react";
 import PageContainers from "../containers/PageContainers"
 import Counter from "../general/Counter"
+import {Rating } from "@mui/material";
+import Button from "../general/Button";
 
 export type CardProductProps = { 
     id: string;
@@ -35,7 +37,9 @@ const DetailClient = ({product} : {product: any}) => {
         if (cardProduct.quantity <= 1) return; 
         setCardProduct(prev => ({ ...prev, quantity: prev.quantity - 1 }));
       };
-      
+
+      let productRanting = product.reviews?.reduce((acc: number, item: any) => acc + item.rating, 0) / product.reviews?.length
+
 
   return (
     <div className="my-10">
@@ -44,14 +48,18 @@ const DetailClient = ({product} : {product: any}) => {
                 <div className="relative h-[400px] w-[200px]">
                     <img src={product?.image} alt={product?.name} />
                 </div>
-                <div className="w-3/4 space-y-3"> 
+                <div className="w-full md:w-3/4 space-y-3"> 
                     <div className="text-xl md:text-2xl"> {product?.name}</div>
+                    <Rating name="read-only" value={productRanting} readOnly />
+
                     <div className="text-slate-500">{product?.description}</div>
                     <div className="flex items-center gap-2">
                         <div>Stok Durumu :</div>
                         {product.inStock ? <div className="text-green-500">Stokta Var</div> : <div className="text-red-500">Stokta Yok</div>}
                     </div>
                 <Counter increaseFunc={increaseFunc} decreaseFunc={decreaseFunc} cardProduct= {cardProduct} />
+                <div className="text-lg md:text-xl text-orange-600 font-bold"> {product?.price} ₺</div>
+                <Button text="Sepete Ekle" small onClick={()=>{}}/>
                 </div>
             </div>
         </PageContainers>
--- app/components/general/Counter.tsx
+++ app/components/general/Counter.tsx
@@ -7,11 +7,13 @@ interface CounterProps {
 }
 
 const Counter:React.FC<CounterProps> = ({ cardProduct,increaseFunc,decreaseFunc}) => {
+  const buttonStyle = "w-8 h-8 border flex items-center justify-center text-lg bg-gray-200 hover:bg-gray-300 cursor-pointer rounded-md p-2";
+
   return (
     <div className="flex items-center gap-5">
-        <div onClick={decreaseFunc}>-</div>
-        <div>{cardProduct?.quantity}</div>
-        <div onClick={increaseFunc}>+</div>
+        <div className={buttonStyle} onClick={decreaseFunc}>-</div>
+        <div className="text-lg md:text-xl font-medium">{cardProduct?.quantity}</div>
+        <div className={buttonStyle} onClick={increaseFunc}>+</div>
     </div>
   )
 }
